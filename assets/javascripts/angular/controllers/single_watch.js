1e5fcdcda3e9b1f0f20db5b81acb7e00d09df4a4
yogeshmurugesan <yogkumit@gmail.com>
2017-12-06 00:02:14 +0530

    search bar added

--- assets/javascripts/angular/controllers/single_watch.js
+++ assets/javascripts/angular/controllers/single_watch.js
@@ -1,17 +1,12 @@
-shareWatch.controller("singleWatchController", function ($scope, $state) {
-  $scope.phones = [
-    {
-      'name': 'Nexus S',
-      'snippet': 'Fast just got faster with Nexus S.'
-    },
-    {
-      'name': 'Motorola XOOM™ with Wi-Fi',
-      'snippet': 'The Next, Next Generation tablet.'
-    },
-    {
-      'name': 'MOTOROLA XOOM™',
-      'snippet': 'The Next, Next Generation tablet.'
-    }
-  ];
-  $state.go('asd')
+shareWatch.controller("singleWatchController", function ($scope) {
+
+  $scope.scrip = {
+    name: null,
+    type: "NSE"
+  };
+
+  $scope.search_scrip = function(){
+    alert(JSON.stringify($scope.scrip));
+  }
+  
 });
